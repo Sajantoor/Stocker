dedaf8561560be0ac278e0bf6148b10721acb2b0
ritam <ritamsingal@gmail.com>
2022-04-30 02:03:28 -0700

    integrated stockers

--- frontend-ionic/stocker/src/views/HomePage.vue
+++ frontend-ionic/stocker/src/views/HomePage.vue
@@ -1,13 +1,14 @@
 <template>
-  <ion-page>
+  <ion-page id="home-page">
 
     <div class="grid">
       <ion-header>
         <ion-img id="logo" :src="require(`../../public/assets/stocker.png`)"></ion-img>
       </ion-header>
 
-      <div>
-        
+      <div class="inner-page">
+        <MyStocks ></MyStocks>
+      <!-- <MyStocks class="inner-page"></MyStocks> -->
       </div>
 
       <ion-tab-bar class="toolbar">
@@ -32,9 +33,13 @@
 <script setup>
 import { IonHeader, IonPage, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue';
 import { trendingUpOutline, map, person } from 'ionicons/icons';
+
+import MyStocks from './components/MyStocks.vue';
+
 </script>
 
 <style scoped>
+
 #logo {
   width: 12em;
   display: block;
@@ -48,6 +53,11 @@ import { trendingUpOutline, map, person } from 'ionicons/icons';
   grid-template-rows: auto 1fr auto;
 }
 
+.inner-page {
+  height: auto;
+  grid-row-start: 2;
+  grid-row-end: 3;
+}
 .toolbar {
   height: 5em;
   grid-row-start: 3;
@@ -55,10 +65,10 @@ import { trendingUpOutline, map, person } from 'ionicons/icons';
 }
 
 .icon-size {
-  font-size: 5em;
+  font-size: 4em;
 }
 
 .icon-label {
-  font-size: 1.5em;
+  font-size: 1.2em;
 }
 </style>
--- frontend-ionic/stocker/src/views/components/MyStocks.vue
+++ frontend-ionic/stocker/src/views/components/MyStocks.vue
@@ -1,44 +1,40 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>My Stocks</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <ion-page> 
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">My Stocks</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <!-- <ion-header collapse="condense">
+      <ion-title size="large">My Stocks</ion-title>
+    </ion-header> -->
+    <h1>
+      My Stocks
+    </h1>
 
-      <ion-searchbar></ion-searchbar>
+    <ion-searchbar></ion-searchbar>
 
+    <ion-content >
       <StockCard></StockCard>
       <StockCard></StockCard>
       <StockCard></StockCard>
       <StockCard></StockCard>
       <StockCard></StockCard>
       <StockCard></StockCard>
+      <StockCard></StockCard>
+      <StockCard></StockCard>
+      <StockCard></StockCard>
+    </ion-content>
 
-      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+    <ion-fab class="add-stock-btn" vertical="bottom" horizontal="end">
       <ion-fab-button>
         <ion-icon :icon="add"></ion-icon>
       </ion-fab-button>
     </ion-fab>
+    
 
-    </ion-content>
   </ion-page>
 </template>
 
 <script setup>
 import {
   IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
   IonSearchbar,
   IonIcon
 
@@ -46,42 +42,30 @@ import {
  import StockCard from '../components/StockCard.vue'
  import { add } from 'ionicons/icons';
 
-
-
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
 
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+h1 {
+  font-size: 2em;
+  font-weight: 700;
+  padding: 0;
+  margin-left: 0.35em;
+  margin-top: 0.5em;
+  margin-bottom: 0;
 }
 
 .stock_price{
   margin-left: auto;
   color: #000;
   font-weight: 900!important;
+}
+
+.stock-list {
+  height: 90%;
+}
+
+.add-stock-btn {
+  margin-bottom: 5em;
 }
 </style>
