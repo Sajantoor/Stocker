8b2096c015ef40657f9eaa1bb8384a65f254839a
ritam <ritamsingal@gmail.com>
2022-04-30 15:42:39 -0700

    updated store username

--- frontend-ionic/stocker/src/store/store.js
+++ frontend-ionic/stocker/src/store/store.js
@@ -4,6 +4,7 @@ export const useStore = defineStore({
   id: 'store',
   
   state: () => ({
+    username: '',
     accessToken: null,
     stocks: [],
   }),
@@ -11,12 +12,18 @@ export const useStore = defineStore({
   actions: {
     setAccessToken(token) {
       this.accessToken = token
+    },
+    setUsername(username) {
+      this.username = username
     }
   },
 
   getters: {
     getAccessToken: (state) => {
       return state.accessToken
+    },
+    getUsername: (state) => {
+      return state.username
     }
   }
 })
