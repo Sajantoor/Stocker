92a07460a65dc138332510983061c9ddf077f59c
Alfonso Ocampo <fonsy.ocampo@gmail.com>
2022-04-30 10:54:40 -0700

    Account Page WIP

--- frontend-ionic/stocker/src/views/HomePage.vue
+++ frontend-ionic/stocker/src/views/HomePage.vue
@@ -2,9 +2,12 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>
+          <center>Stocker Logo</center>
+          </ion-title>
       </ion-toolbar>
     </ion-header>
+  
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
@@ -13,31 +16,184 @@
         </ion-toolbar>
       </ion-header>
     
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <div class="Header">
+          <font face="Arial" size="5px">
+            <strong>
+              <padding>
+                My Account
+              </padding>
+              </strong>
+          </font>
+      </div>
+
+      <div class="AccountInfo">
+        <ion-card>
+          <ion-card-header>           
+          </ion-card-header>
+        <ion-card-content>
+        <div class="userData1">
+            <div class="profilePic">
+                  <ion-avatar>
+                   <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y">
+                  </ion-avatar>        
+            </div>
+            <p>
+             <strong>Username</strong>
+            </p>
+        </div>
+        
+        <div class="userData2">
+            <div class="email"> 
+              <p>
+                <strong>Email</strong>
+              </p>
+            </div>
+        </div>
+        </ion-card-content>
+
+        <div class="stocks">
+        <h1><strong>Top Stocks</strong></h1>
+        </div>
+        <ion-card>
+            <ion-card-header>
+            </ion-card-header>
+            <ion-card-content>
+                stocks
+            </ion-card-content>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+          </ion-card-header>
+          <ion-card-content>
+              stocks
+          </ion-card-content>
+        </ion-card>
+
+        <ion-card>
+          <ion-card-header>
+          </ion-card-header>
+          <ion-card-content>
+              stocks
+          </ion-card-content>
+        </ion-card>
+        </ion-card>
+      </div>
+
+
+      <div class="list">
+      <ion-list>
+          <ion-item>
+            <ion-label>
+              <ion-icon :icon="notificationsOutline" slot="start" size="small"></ion-icon>
+              &ensp;Notifications
+            </ion-label>
+            <ion-toggle slot="end"></ion-toggle>
+          </ion-item>
+
+          <ion-item>
+            <ion-label>
+              <ion-icon :icon="pinOutline" slot="start" size="small"></ion-icon>
+              &ensp;Turn Locations On
+              </ion-label>
+            <ion-radio slot="end"></ion-radio>
+          </ion-item>
+
+         <ion-item>
+            <ion-label>
+              <ion-icon :icon="personOutline" slot="start" size="small"></ion-icon>
+              &ensp;Privacy
+            </ion-label>
+            <ion-icon :icon="chevronForwardOutline" slot="end"></ion-icon>
+          </ion-item>
+
+          <ion-item>
+            <ion-label>
+              <ion-icon :icon="constructOutline" slot="start" size="small"></ion-icon>
+              &ensp;Security
+            </ion-label>
+            <ion-icon :icon="chevronForwardOutline" slot="end"></ion-icon>
+          </ion-item>
+
+          <ion-item>
+            <ion-label>
+              <ion-icon :icon="flagOutline" slot="start" size="small"></ion-icon>
+              &ensp;Lorem ipsum
+            </ion-label>
+            <ion-icon :icon="chevronForwardOutline" slot="end"></ion-icon>
+          </ion-item>
+
+          <ion-item>
+            <ion-icon :icon="chevronForwardOutline" slot="end"></ion-icon>
+            <ion-label>
+              Logout 
+            </ion-label>
+          </ion-item>
+
+          </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script setup>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/vue';
+import { chevronForwardOutline, notificationsOutline, pinOutline, personOutline,constructOutline, flagOutline } from 'ionicons/icons'  ;
 
-export default defineComponent({
-  name: 'HomePage',
-  components: {
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  }
-});
+// import { defineComponent } from 'vue';
+
+// export default defineComponent({
+//   name: 'HomePage',
+//   components: {
+//     IonContent,
+//     IonHeader,
+//     IonPage,
+//     IonTitle,
+//     IonToolbar,
+//     IonIcon,
+//     chevronForwardOutline
+//   }
+// });
 </script>
 
 <style scoped>
+.Header{
+  padding-top: 20px;
+  padding-left: 20px
+}
+
+.list{
+  
+}
+
+.userData1{
+  display: flex;
+  gap: 40px;
+  flex-flow: row;
+  height: 50px;
+  align-content: space-around;
+}
+
+.userData2{
+  display: flex;
+  gap: 10px;
+  flex-flow: column;
+  height: 10px;
+  align-content: space-between;
+  justify-content: space-around;
+}
+
+.profilePic{
+  padding-left: 15px;
+}
+
+.email{
+  padding-left: 120px;
+}
+
+.stocks{
+  padding-left: 20px;
+}
 #container {
   text-align: center;
   
